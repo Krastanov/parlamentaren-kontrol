321c4c05af97de739babfcde1372919557dba801
Stefan Krastanov <krastanov.stefan@gmail.com>
2012-07-27 01:14:43 +0200

    Clean-up, integration and first steps in presenting the info.

--- start_over.sh
+++ start_over.sh
@@ -1,5 +1,10 @@
 rm -rf data
 mkdir data
+echo "Update all periods for which there is a record."
 sh update_all_periods.sh
+echo "Update all IDs of plenary stenograms."
 sh update_IDs_plenary_stenograms_after_2012.sh
-python stenograms_to_db.py
+echo "Download and parse the stenograms."
+python stenograms_to_db.py > /dev/null
+echo "Print a sample from the data base."
+python load_stenograms_db.py

f667f5f884ce917035e42bdea7a8eae622c919fd
Stefan Krastanov <krastanov.stefan@gmail.com>
2012-09-06 00:14:32 +0200

    Change DB format and add htmlkickstart to the html templates.

--- stenograms_to_db.py
+++ stenograms_to_db.py
@@ -5,12 +5,34 @@ import xlrd
 import shelve
 
 
-################
-###
-### HTML PARSING
-###
-################
+##############################################################################
+# Data Containers
+##############################################################################
+stgram_tuple = namedtuple('stgram_tuple', ['date', 'text_lines',
+                                           'reg_by_name_dict',
+                                            # {name_string: registered_bool}
+                                           'reg_by_party_dict',
+                                            # {name_string: reg_stats_per_party_tuple}
+                                           'sessions',
+                                            # [sesion_tuple]
+                                           ])
 
+rep_tuple = namedtuple('rep_tuple', ['name', 'party'])
+
+session_tuple = namedtuple('session_tuple', ['description',
+                                             'votes_by_name_dict',
+                                                # {name_string: vote_code_string}
+                                             'votes_by_party_dict',
+                                                # {name_string: vote_stats_per_party_tuple}
+                                             ])
+
+reg_stats_per_party_tuple = namedtuple('reg_stats_per_party_tuple', ['present', 'expected'])
+vote_stats_per_party_tuple = namedtuple('vote_stats_per_party_tuple', ['yes', 'no', 'abstained', 'total'])
+
+
+##############################################################################
+# HTML Parsing
+##############################################################################
 class StenogramsHTMLParser(HTMLParser):
     # TODO text_lines does not contain all the content
 
@@ -43,25 +65,11 @@ class StenogramsHTMLParser(HTMLParser):
             self.data_list.append(data)
 
 
-stgram = namedtuple('stgram', ['date', 'text_lines', 'by_name_votes', 'by_party_votes'])
-
-
-#################
-###
-### EXCEL PARSING
-###
-#################
-
-# TODO rename these so it is obvious that they are containers
-# for instance a suffix _tuple
-rep = namedtuple('rep', ['name', 'party'])
-session = namedtuple('session', ['kind', 'details'])
-reg_stats = namedtuple('reg_stats', ['present', 'expected'])
-vote_stats = namedtuple('vote_stats', ['yes', 'no', 'abstained', 'total'])
-
-registration = u'\u0420\u0415\u0413\u0418\u0421\u0422\u0420\u0410\u0426\u0418\u042f'
-vote = u'\u0413\u041b\u0410\u0421\u0423\u0412\u0410\u041d\u0415'
-
+##############################################################################
+# Excel Parsing
+##############################################################################
+registration_marker = u'\u0420\u0415\u0413\u0418\u0421\u0422\u0420\u0410\u0426\u0418\u042f'
+vote_marker = u'\u0413\u041b\u0410\u0421\u0423\u0412\u0410\u041d\u0415'
 parties_count = 6
 
 def parse_excel_by_name(filename):
@@ -78,15 +86,6 @@ def parse_excel_by_name(filename):
         - representative's party
         - undefined number of fields containing stuff about how the
         representative voted.
-
-    Returns
-    =======
-
-    The result is a
-        dictionary:
-            key: a namedtuple containing the name and party of the representative
-            value: a tuple containing the vote statistics for this representative
-
     """
     book = xlrd.open_workbook(filename)
     sheet = book.sheet_by_index(0)
@@ -97,7 +96,7 @@ def parse_excel_by_name(filename):
     for row in range(init_row, rows):
         name = sheet.cell_value(rowx=row, colx=0).strip().upper()
         party = sheet.cell_value(rowx=row, colx=3).strip().upper()
-        key = rep(name=name, party=party)
+        key = rep_tuple(name=name, party=party)
         value = []
         for col in range(4, cols):
             value.append(sheet.cell_value(rowx=row, colx=col))
@@ -109,58 +108,42 @@ def parse_excel_by_party(filename):
     u"""
     Parse excel files with vote statistics by party.
 
+    One excel file per stenogram.
+
     Assumptions
     ===========
 
     - There is a total of six parties (see parties_count above)
-    - For each session, there is a line containing either "GLASUVANE" or
-    "REGISTRACIA", and that's how we know what's going on
+    - For each session, there is a line containing either `vote_marker` or
+    `registration_marker`, and that gives the kind of the session. There is
+    only one registration per stenogram.
     - After this line, there are two lines we don't care about, and the next
     parties_count consecutive lines contain the vote/presence statistics by party.
-
-    Returns
-    =======
-
-    The result is a
-        dictionary:
-            key: namedtuple describing the session type and additional details
-            value:
-                dictionary:
-                    key: party
-                    value:
-                        namedtuple:
-                            key: present/expected for 'REGISTRACIA', or
-                                 for/against/skipped/total for 'GLASUVANE'
-                            value: corresponding numbers in int format
 
     """
     book = xlrd.open_workbook(filename)
     sheet = book.sheet_by_index(0)
     cols = sheet.ncols
     rows = sheet.nrows
-    result = OrderedDict()
+    sessions_dict = OrderedDict()
     row = 0
     while row < rows:
         first = sheet.cell_value(rowx=row, colx=0)
-        if registration in first:
-            kind = registration
-            details = first.split(registration)[-1].strip()
-            key = session(kind=kind, details=details)
+        if registration_marker in first:
+            details_str = first.split(registration_marker)[-1].strip()
             row += 2
-            value = {}
+            per_party_dict = {}
             for i in range(parties_count):
                 row += 1
                 party = sheet.cell_value(rowx=row, colx=0).strip().upper()
                 present = int(sheet.cell_value(rowx=row, colx=1))
                 expected = int(sheet.cell_value(rowx=row, colx=2))
-                value[party] = reg_stats(present=present, expected=expected)
-            result[key] = value
-        elif vote in first:
-            kind = vote
-            details = first.split(vote)[-1].strip()
-            key = session(kind=kind, details=details)
+                per_party_dict[party] = reg_stats_per_party_tuple(present, expected)
+            reg_by_party_dict = per_party_dict
+        elif vote_marker in first:
+            details_str = first.split(vote_marker)[-1].strip()
             row += 2
-            value = {}
+            per_party_dict = {}
             for i in range(parties_count):
                 row += 1
                 party = sheet.cell_value(rowx=row, colx=0).strip().upper()
@@ -168,57 +151,55 @@ def parse_excel_by_party(filename):
                 no = int(sheet.cell_value(rowx=row, colx=2))
                 abstained = int(sheet.cell_value(rowx=row, colx=3))
                 total = int(sheet.cell_value(rowx=row, colx=4))
-                value[party] = vote_stats(yes=yes, no=no, abstained=abstained, total=total)
-            result[key] = value
+                per_party_dict[party] = vote_stats_per_party_tuple(yes, no, abstained, total)
+            sessions_dict[details_str] = per_party_dict
         row += 1
-    return result
-
-def pprint_result_by_party(result):
-    """
-    Print the output of the parser for results by party.
-
-    So that it makes more sense.
-
-    """
-    for key, value in result.items():
-        print key.kind
-        print key.details
-        for value_key, value_value  in value.items():
-            print value_key, ':', value_value
+    return reg_by_party_dict, sessions_dict
 
 
-
-
+##############################################################################
+# Parse and save to disc.
+##############################################################################
 if __name__ == '__main__':
     stenograms = {}
     stenogram_IDs = open('data/IDs_plenary_stenograms').readlines()
     for ID in stenogram_IDs[:2]:
         ID = ID.strip()
-        print "At ID: ", ID
+        print "###########"
+        print "#### At ID: ", ID
+        print "###########"
+        print "- downloading and parsing HTML data"
         parser = StenogramsHTMLParser()
         f = urlopen('http://www.parliament.bg/bg/plenaryst/ID/'+ID)
         parser.feed(f.read().decode('utf-8'))
-        print parser.date
 
         day, month, year = parser.date.split('/')# TODO the next 3 lines should use the datetype
         date_string = day + month + year[2:]
 
+        print "- downloading and parsing votes-by-name excel data"
         by_name_temp = open('data/temp_name.excel', 'wb')
         by_name_web = urlopen("http://www.parliament.bg/pub/StenD/iv%s.xls" % date_string)
         by_name_temp.write(by_name_web.read())
         by_name_temp.close()
         by_name_dict = parse_excel_by_name('data/temp_name.excel')
 
+        print "- downloading and parsing votes-by-party excel data"
         by_party_temp = open('data/temp_party.excel', 'wb')
         by_party_web = urlopen("http://www.parliament.bg/pub/StenD/gv%s.xls" % date_string)
         by_party_temp.write(by_party_web.read())
         by_party_temp.close()
-        by_party_dict = parse_excel_by_party('data/temp_party.excel')
+        reg_by_party_dict, sessions_dict = parse_excel_by_party('data/temp_party.excel')
 
-        stenograms[ID]=stgram(date=parser.date,
-                              text_lines=parser.data_list,
-                              by_name_votes=by_name_dict,
-                              by_party_votes=by_party_dict)
+        sessions = [session_tuple(description,
+                                  None,
+                                  votes_by_party)
+                    for (description, votes_by_party) in sessions_dict.items()]
+
+        stenograms[ID]=stgram_tuple(parser.date,
+                                    parser.data_list,
+                                    None,
+                                    reg_by_party_dict,
+                                    sessions)
 
     stenograms_dump = shelve.open('data/stenograms_dump')
     stenograms_dump['stenograms'] = stenograms

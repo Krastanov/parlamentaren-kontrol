6f24eee528a9d28c35ca4aa05c9027521713e646
Stefan Krastanov <krastanov.stefan@gmail.com>
2012-09-06 13:26:07 +0200

    More work on listing the stenograms and generating HTML.

--- create_html_pages.py
+++ create_html_pages.py
@@ -1,4 +1,5 @@
 from mako.template import Template
+from mako.lookup import TemplateLookup
 from stenograms_to_db import *
 import shelve
 import os
@@ -10,13 +11,16 @@ os.system('cp htmlkickstart/style.css generated_html/style.css')
 stenograms_dump = shelve.open('data/stenograms_dump')
 stenograms = stenograms_dump['stenograms']
 
-per_stenogram_template = Template(filename='per_stenogram_template.html',
-                                  input_encoding='utf-8',
-                                  output_encoding='utf-8')
+templates = TemplateLookup(directories=['mako_templates'],
+                           input_encoding='utf-8',
+                           output_encoding='utf-8')
 
+per_stenogram_template = templates.get_template('stenogramN_template.html')
 for k, st in stenograms.items():
-    html_file = open('generated_html/stenogram%s.html'%k, 'w')
-    html_file.write(per_stenogram_template.render(stenogram=st))
+    with open('generated_html/stenogram%s.html'%k, 'w') as html_file:
+        html_file.write(per_stenogram_template.render(stenogram=st))
 
-
+all_stenograms_template = templates.get_template('stenograms_template.html')
+with open('generated_html/stenograms.html', 'w') as html_file:
+    html_file.write(all_stenograms_template.render(stenograms=stenograms))
 

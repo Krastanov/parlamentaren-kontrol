21f7d769a21e5299bd634a14bb8fe190c2df533b
Stefan Krastanov <krastanov.stefan@gmail.com>
2012-09-08 02:51:49 +0200

    Include 2011 by default and switch to cPickle from shelve for the DB.

--- create_html_pages.py
+++ create_html_pages.py
@@ -1,24 +1,25 @@
 from mako.template import Template
 from mako.lookup import TemplateLookup
 from stenograms_to_db import *
-import shelve
 import os
+import cPickle
 
 os.system('cp -r htmlkickstart/css generated_html/css')
 os.system('cp -r htmlkickstart/js generated_html/js')
 os.system('cp htmlkickstart/style.css generated_html/style.css')
 
-stenograms_dump = shelve.open('data/stenograms_dump')
-stenograms = stenograms_dump['stenograms']
+stenograms_dump = open('data/stenograms_dump', 'r')
+stenograms = cPickle.load(stenograms_dump)
+stenograms_dump.close()
 
 templates = TemplateLookup(directories=['mako_templates'],
                            input_encoding='utf-8',
                            output_encoding='utf-8')
 
 per_stenogram_template = templates.get_template('stenogramN_template.html')
-#for st in stenograms.values():
-#    with open('generated_html/stenogram%s.html'%st.date.strftime('%Y%m%d'), 'w') as html_file:
-#        html_file.write(per_stenogram_template.render(stenogram=st))
+for st in stenograms.values():
+    with open('generated_html/stenogram%s.html'%st.date.strftime('%Y%m%d'), 'w') as html_file:
+        html_file.write(per_stenogram_template.render(stenogram=st))
 
 all_stenograms_template = templates.get_template('stenograms_template.html')
 with open('generated_html/stenograms.html', 'w') as html_file:
